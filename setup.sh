a546f912854da2a33118c3bc060132ce2b7eb608
Dominic Mitchell <dom@happygiraffe.net>
2009-09-10 15:50:59 +0100

    Replace backticks with $()
    They nest better and are easier to read.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@
 # The user can, of course, choose to override the values the script digs up for user.name and user.email
 
 
-user=`git config --global github.user`
-token=`git config --global github.token`
+user=$(git config --global github.user)
+token=$(git config --global github.token)
 
 
 if [ -z "$user" ]; then
@@ -21,13 +21,13 @@ if [ -z "$token" ]; then
 	echo "" # Because we didn't echo the user's return key above
 
 	echo "Fetching API token"
-	acct=`curl https://github.com/account --user $user:$password 2> /dev/null`
-	token=`echo "$acct" | grep "API Token" | sed "s/.*API Token: <strong>\(.*\)<.strong>.*/\1/"`
+	acct=$(curl https://github.com/account --user $user:$password 2> /dev/null)
+	token=$(echo "$acct" | grep "API Token" | sed "s/.*API Token: <strong>\(.*\)<.strong>.*/\1/")
 
 	if [ $token ]; then
 		echo "Saving GitHub token to global git config"
-		`git config --global github.user $user`
-		`git config --global github.token $token`
+		$(git config --global github.user $user)
+		$(git config --global github.token $token)
 	else
 		echo "Error retrieving token"
 		exit 1
@@ -36,12 +36,12 @@ fi
 
 if [ -z "$acct" ]; then
 	echo "Fetching GitHub account details"
-	acct=`curl -F "login=$user" -F "token=$token" https://github.com/account 2> /dev/null`
+	acct=$(curl -F "login=$user" -F "token=$token" https://github.com/account 2> /dev/null)
 fi
 
 
 # Setup username
-gitname=`git config --global user.name`
+gitname=$(git config --global user.name)
 if [ -z "$gitname" ]; then
 	gitname=$user
 fi
@@ -50,12 +50,12 @@ read -p "Enter git committer name (return to use '$gitname'): " -e newgitname
 if [ -z "$newgitname" ]; then
 	newgitname=$gitname
 fi
-`git config --global user.name "$newgitname"`
+$(git config --global user.name "$newgitname")
 
 
 # Setup email
-ghemail=`echo "$acct" | grep 'class="address"' | head -n1 | sed "s/.*>\([^@]*@[^<]*\)<.*/\1/"`
-gitemail=`git config --global user.email`
+ghemail=$(echo "$acct" | grep 'class="address"' | head -n1 | sed "s/.*>\([^@]*@[^<]*\)<.*/\1/")
+gitemail=$(git config --global user.email)
 if [ -z "$gitemail" ]; then
 	gitemail=$ghemail
 fi
@@ -64,7 +64,7 @@ read -p "Enter git committer email (return to use '$gitemail'): " -e newgitemail
 if [ -z "$newgitemail" ]; then
 	newgitemail=$gitemail
 fi
-`git config --global user.email "$newgitemail"`
+$(git config --global user.email "$newgitemail")
 
 
 # SSH keys!
@@ -86,15 +86,15 @@ if [ -f ~/.ssh/id_rsa ]; then
 	read -n1 -p "Upload id_rsa key to your GitHub account? (y/n) "
 	echo ""
 	if [[ $REPLY = [yY] ]]; then
-		sshkey=`cat ~/.ssh/id_rsa.pub`
-		acct=`curl -F "login=$user" -F "token=$token" https://github.com/account/public_keys -F "public_key[key]=$sshkey" 2> /dev/null`
+		sshkey=$(cat ~/.ssh/id_rsa.pub)
+		acct=$(curl -F "login=$user" -F "token=$token" https://github.com/account/public_keys -F "public_key[key]=$sshkey" 2> /dev/null)
 	fi
 fi
 
 
 # ssh-agent helper for msysgit
 if [ $MSYSTEM ]; then
-	script_installed=`grep "source ~/.ssh/agent-loader" ~/.bashrc 2> /dev/null`
+	script_installed=$(grep "source ~/.ssh/agent-loader" ~/.bashrc 2> /dev/null)
 	if [ -z "$script_installed" ]; then
 		echo ""
 		echo "You appear to be running Msysgit, would you like to use the ssh-agent loader?"
